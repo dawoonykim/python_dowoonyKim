5ad55f762fb7bd4ada085808ecec8930703e9b57
dawoonykim <dawoonk111@gmail.com>
2024-11-29 13:55:57 +0900

    자료형 전까지

---
+++ 24.11.29/practice1.py
@@ -0,0 +1,25 @@
+# # print("Hello World!")
+# print("Hello", "Python")
+# print("Hello", "Python", 1, 2, "asdfe")
+# print("Hello", "Python", 1, 2, "asdfe", sep="\\")
+# print("Hello", "Python", 1, 2, "asdfe", sep="\n")
+# print("Hello", "World!", sep="")
+# print()
+# print("1")
+# print("안녕하세요.", end="")
+# print("코딩온입니다.")
+
+# name = "홍길동"
+# age = 30
+# print(f"나의 이름은 {name}입니다. 나이는 {age}입니다.")
+# print(f"나는 내년이면 {age+1}살이 된다.")
+
+ive = "I AM"
+print(ive)
+ive = "장원영"
+print(ive)
+
+# 변수명을 짓는 방법
+# my_home : sanke_case
+# MyHome : CamelCase
+# myHome : PascalCase
