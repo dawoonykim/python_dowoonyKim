aa273ed81d18386b2be6a441c2efb6924e9716fd
dawoonykim <dawoonk111@gmail.com>
2024-11-30 13:50:49 +0900

    24.11.30

--- 24.11.30/test4.py
+++ 24.11.30/test4.py
@@ -8,6 +8,10 @@ print(rainbow)
 # 3. 좋아하는 색 맨 마지막에 추가하기
 rainbow.append("magenta")
 print(rainbow)
-# 4. 4~6 번 째 값 삭제하기
+# 4. 3~6 번 째 값 삭제하기
 del rainbow[3:7]
 print(rainbow)
+
+print(rainbow.index("magenta"))
+print(rainbow.pop())
+print(rainbow.count("red"))
